54d514a1790c8c43bb46d6bc071918748e6bee91
flipperblack <marco.gomez@davinci.edu.ar>
2024-05-06 19:49:21 -0300

    refactor: limpieza de imports no utilizados en main activity

--- app/src/main/java/com/example/grupofacil/MainActivity.java
+++ app/src/main/java/com/example/grupofacil/MainActivity.java
@@ -1,21 +1,14 @@
 package com.example.grupofacil;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.graphics.Typeface;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
-import androidx.core.content.ContextCompat;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
 
